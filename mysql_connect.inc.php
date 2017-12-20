74e46a0d5cdb91042f6479e3aac822009bf64fcd
USER <104021097@live.asia.edu.tw>
2017-12-20 11:48:36 +0800

    1

--- mysql_connect.inc.php
+++ mysql_connect.inc.php
@@ -1,7 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 
-$link = mysqli_connect("localhost","104021097","#3mjF3uFV","104021097");
+$link = mysqli_connect("localhost","104021097","#3mjF3uFV","104021097_");
 if(!$link){
     echo "Error:Unable to connect to MySQL.".PHP_EOL;
     echo "Debugging error:".mysqli_connect_error().PHP_EOL;
